2f6abc2f6830245f89fc84286cec7455063143a2
creativebcx <ericdonley1@gmail.com>
2017-05-20 16:30:21 -0600

    update-w-luis

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function displayYoutubeSearchData(data) {
      resultElement += '<a href="'+item.snippet.thumbnails.default.url+'"><img class="thumbnails" src="' + item.snippet.thumbnails.default.url + '"/></a>';
     });
   }
-  //use <a element, open and close in different
+
   else {
     resultElement += '<p>No results</p>';
   }
@@ -30,3 +30,8 @@ function init() {
 }
 
 $(function(){init();});
+
+//wrap them again, width that lines to edge, margin: 0px auto,
+
+//tablet size do grid with 2x2 spacing - grid spacing for phone 
+
