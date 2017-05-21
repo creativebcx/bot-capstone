e95939476a56f1095368a725088a8c3cb6e54042
creativebcx <ericdonley1@gmail.com>
2017-05-21 08:13:56 -0600

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function displayYoutubeSearchData(data) {
   var resultElement = '';
   if (data.items) {
     data.items.forEach(function(item) {
-     resultElement += '<a href="'+item.snippet.thumbnails.default.url+'"><img class="thumbnails" src="' + item.snippet.thumbnails.default.url + '"/></a>';
+     resultElement += '<source src="'+item.snippet.thumbnails.default.url+'"type="video/mp4"><img class="thumbnails" src="'+ item.snippet.thumbnails.default.url +'"/>';
     });
   }
 
@@ -21,7 +21,7 @@ function displayYoutubeSearchData(data) {
 
 function init() {
   var query = {
-    q: 'jackson hole',
+    q: 'yellowstone and teton summer outdoor adventures',
     r: 'json',
     key: 'AIzaSyChuFTyDgp6IMVOfnb03oDUBZw1qC7rCGA',
     part: 'snippet'
@@ -34,4 +34,3 @@ $(function(){init();});
 //wrap them again, width that lines to edge, margin: 0px auto,
 
 //tablet size do grid with 2x2 spacing - grid spacing for phone 
-
